f6d084975fe177cee0f30ba503845f32cb70a2cc
ondogdog1231 <15054849d@connect.polyu.hk>
2019-01-15 02:00:32 +0800

    first commit

--- crawler.py
+++ crawler.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-
 import requests
 import re
 import time
@@ -12,11 +11,13 @@ import json
 # from library.bs4 import BeautifulSoup
 from bs4 import BeautifulSoup
 
-
-
 url = "http://liansai.500.com/zuqiu-4826/"
-
 
 r = requests.get(url)
 
-print r.content.decode("gbk").encode("utf-8")
+content = r.content.decode("gbk").encode("utf-8")
+
+# print content
+f = open("./result.txt", "w+")
+f.write(content)
+f.close()
