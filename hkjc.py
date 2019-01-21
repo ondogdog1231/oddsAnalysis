2213f3706d04b82e2f429ba3d28fb09a4a563007
adam.lok <adadm.lok@paymentasai.com>
2019-01-21 18:13:34 +0800

    w

--- hkjc.py
+++ hkjc.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-
 import requests
 import re
 import time
@@ -12,11 +11,59 @@ import json
 # from library.bs4 import BeautifulSoup
 from bs4 import BeautifulSoup
 
+import datetime
+
+mylist = [x*x for x in range(3)]
+for i in mylist:
+    print(i)
+""""""
+exit()
+
+def daterange(start_date, end_date):
+    if start_date != end_date:
+        for n in range((end_date - start_date).days + 1):
+            yield start_date + datetime.timedelta(n)
+    else:
+        for n in range((start_date - end_date).days + 1):
+            yield start_date - datetime.timedelta(n)
 
 
-url = "https://bet.hkjc.com/football/getJSON.aspx?jsontype=search_result.aspx&startdate=20181222&enddate=20190121&teamid=default"
+start = datetime.date(year=2010, month=1, day=1)
+end = datetime.date(year=2018, month=2, day=1)
+print start
+
+for date in daterange(start, end):
+    print date
+exit()
 
 
-r = requests.get(url)
+def jsonCheck(m):
+    try:
+        json.loads(m)
+        return 1
+    except:
+        return 2
 
-print r.content.decode("gbk").encode("utf-8")
+
+def sessionGet():
+    sessionUrl = "https://bet.hkjc.com/football/"
+    s = requests.Session()
+    sessionSet = s.get(sessionUrl)
+    return s
+
+
+failCount = 0
+
+s = sessionGet()
+time.sleep(1)
+url = "https://bet.hkjc.com/football/getJSON.aspx?jsontype=search_result.aspx&startdate=20180101&enddate=20180131&teamid=default&pageno=40"
+
+content = s.get(url)
+resultJson = json.loads(content)
+print resultJson.content
+exit()
+
+f = open("./result.txt", "w+")
+f.write(resultJson.content)
+f.close()
+exit()
