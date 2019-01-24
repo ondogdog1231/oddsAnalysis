e1c435b1f524e488175e3ffd4920dcd5b9ea7370
ondogdog1231 <15054849d@connect.polyu.hk>
2019-01-24 10:45:31 +0800

    wdn

--- analysis/matchAnalysis.py
+++ analysis/matchAnalysis.py
@@ -50,9 +50,25 @@ drawScoreOddlist = {
     "S0303": "3_3",
     "SM1MD": "others",
 }
+upCount = 0
+downCount = 0
+fileCount = 0
+hot_1_5_Count = 0
+hot_1_6_Count = 0
+hot_1_7_Count = 0
+hot_1_8_Count = 0
+fileNameList = []
 
-for filenames in os.listdir("../matchDetailResult"):
-    print filenames
+for root, subdirs, files in os.walk("../matchDetailResult"):
+
+    for fileName in files:
+        path = "%s/%s" % (root, fileName)
+        fileNameList.append(path)
+# print fileNameList
+
+
+for filenames in fileNameList:
+
     handicap = {}
     scoreOdds = {
         "home": {},
@@ -68,8 +84,8 @@ for filenames in os.listdir("../matchDetailResult"):
     # print "%s vs %s" % (matchJson["homeTeam"]["teamNameCH"], matchJson["awayTeam"]["teamNameCH"])
 
     if matchJson["matchStatus"] != "ResultIn":
-        print "%s vs %s" % (matchJson["homeTeam"]["teamNameCH"], matchJson["awayTeam"]["teamNameCH"])
-        print "is canceled"
+        # print "%s vs %s" % (matchJson["homeTeam"]["teamNameCH"], matchJson["awayTeam"]["teamNameCH"])
+        # print "is canceled"
         continue
     # Euro Odds
     if "hadodds" in matchJson:
@@ -79,8 +95,8 @@ for filenames in os.listdir("../matchDetailResult"):
 
     # Asia Handicap
     if "hdcodds" in matchJson:
-        handicap["HomeOdds"] = matchJson["hdcodds"]["H"]
-        handicap["AwayOdds"] = matchJson["hdcodds"]["A"]
+        handicap["HomeOdds"] = re.findall("@(\d+.\d{1,2})", matchJson["hdcodds"]["H"])[0]
+        handicap["AwayOdds"] = re.findall("@(\d+.\d{1,2})", matchJson["hdcodds"]["A"])[0]
         # Handicap["Handicap"] = matchJson["hdcodds"]["AG"]
         HG = str(matchJson["hdcodds"]["HG"]).split("/")
         handicap["homeHandicap"] = float((float(HG[0]) + float(HG[1])) / 2)
@@ -97,17 +113,16 @@ for filenames in os.listdir("../matchDetailResult"):
 
     homeScoreProb, drawScoreProb, awayScoreProb = 0.00, 0.00, 0.00
     for (k, v) in scoreOdds["home"].iteritems():
-        homeScoreProb = homeScoreProb + 1 / float(v)
+        homeScoreProb = homeScoreProb + (1 / float(v))
     for (k, v) in scoreOdds["draw"].iteritems():
-        drawScoreProb = drawScoreProb + 1 / float(v)
+        drawScoreProb = drawScoreProb + (1 / float(v))
     for (k, v) in scoreOdds["away"].iteritems():
-        awayScoreProb = awayScoreProb + 1 / float(v)
-
+        awayScoreProb = awayScoreProb + (1 / float(v))
 
     summary = {
         "homeTeam": matchJson["homeTeam"]["teamNameCH"],
         "awayTeam": matchJson["awayTeam"]["teamNameCH"],
-        "matchTime": matchJson["matchTime"],
+        "matchTime": re.findall("(\d+-\d{1,2}-\d{1,2}T\d{1,2}:\d{1,2})", matchJson["matchTime"])[0].replace("T", " "),
         "euro": euroOdds,
         "asia": handicap,
         "prob": {
@@ -115,9 +130,43 @@ for filenames in os.listdir("../matchDetailResult"):
             "draw": drawScoreProb,
             "away": awayScoreProb,
         },
-        "homeScore": matchJson["accumulatedscore"][1]["home"],
-        "awayScore": matchJson["accumulatedscore"][1]["away"],
+        "homeScore": int(matchJson["accumulatedscore"][1]["home"]),
+        "awayScore": int(matchJson["accumulatedscore"][1]["away"]),
     }
+    if summary["asia"] is not None:
+        if summary["asia"]["homeHandicap"] == -0.75:
+            if summary["homeScore"] + -0.75 - summary["awayScore"] > 0.0:
+                summary["asiaResult"] = "UP"
+            else:
+                summary["asiaResult"] = "DOWN"
 
-    if summary["asia"] is not None and abs(summary["asia"]["homeHandicap"]) == 0.25:
-        print(json.dumps(summary, indent=4, sort_keys=False ,encoding='UTF-8', ensure_ascii=False))
+        if summary["asia"]["homeHandicap"] == 0.75:
+            if summary["homeScore"] + 0.75 - summary["awayScore"] > 0.0:
+                summary["asiaResult"] = "DOWN"
+            else:
+                summary["asiaResult"] = "UP"
+
+    if summary["asia"] is not None and abs(summary["asia"]["homeHandicap"]) == 0.75:
+        # print(json.dumps(summary, indent=4, sort_keys=False, encoding='UTF-8', ensure_ascii=False))
+
+        fileCount = fileCount + 1
+        lowerLimit = 0.888
+        upperLimit = 0.90
+        # if (float(summary["prob"]["draw"]) >= 0.3):
+
+        if (float(summary["prob"]["home"]) >= lowerLimit and float(summary["prob"]["home"]) <= upperLimit) or (
+                        float(summary["prob"]["away"]) >= lowerLimit and float(summary["prob"]["away"]) <= upperLimit):
+            # print(json.dumps(summary, indent=4, sort_keys=False, encoding='UTF-8', ensure_ascii=False))
+
+            if summary["asiaResult"] is "UP":
+                upCount = upCount + 1
+                # print "%s-%s" % (summary["homeScore"],summary["awayScore"])
+
+            if summary["asiaResult"] is "DOWN":
+                downCount = downCount + 1
+            print filenames
+            print(json.dumps(summary, indent=4, sort_keys=False, encoding='UTF-8', ensure_ascii=False))
+
+print "File count: %s" % fileCount
+print "Up count: %s" % upCount
+print "Down count: %s" % downCount
