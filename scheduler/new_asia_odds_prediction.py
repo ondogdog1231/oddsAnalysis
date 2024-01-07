d2aa1e331f4b6d7c0eeb7c5762974906e414c737
ondogdog1231 <15054849d@connect.polyu.hk>
2024-01-07 09:58:01 +0800

    remove debug log

--- scheduler/new_asia_odds_prediction.py
+++ scheduler/new_asia_odds_prediction.py
@@ -176,7 +176,6 @@ for matchId in oddSummaryList.keys():
 # print(svmMatchIDList[21])
 for league_id in svmTrainList.keys():
     print(league_id)
-    continue
     print(matchList)
     print(svmMatchIDList[league_id])
     keys_to_remove = list(set(matchList) - set(svmMatchIDList[league_id]))
