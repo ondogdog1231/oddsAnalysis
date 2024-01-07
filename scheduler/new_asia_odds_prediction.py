9fb067036a35095cf8def28d8df996a59257f82b
ondogdog1231 <15054849d@connect.polyu.hk>
2024-01-07 09:20:22 +0800

    test for leagueID not exist in json model list

--- scheduler/new_asia_odds_prediction.py
+++ scheduler/new_asia_odds_prediction.py
@@ -166,8 +166,9 @@ for matchId in oddSummaryList.keys():
         # svmMatchList.append(_matchFibonacciKeyValue)
     svmTrainList[league_id].append(_matchFibonacciKeyValue)
     svmMatchIDList[league_id].append(matchId)
-# print("svmMatchIDList")
-# print(matchList)
+print("svmMatchIDList")
+print(matchList)
+exit()
 # print(svmMatchIDList[21])
 for league_id in svmTrainList.keys():
     print(league_id)
